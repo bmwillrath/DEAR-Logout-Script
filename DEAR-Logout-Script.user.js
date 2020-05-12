1f13b4e4466e08e5a0e27183c02b76643be4b694
Braydan Willrath <bmwillrath@gmail.com>
2020-05-12 10:56:39 +1000

    Added Quick-Switch User Icon
    Fixed issue where after logout and login dropdown still displayed
    Fixed issue of logout during payment screen if parked sale was restored
    Fixed issue not logging out after payment has been processed
    Added Quick-Switch User Icon

--- DEAR-Logout-Script.user.js
+++ DEAR-Logout-Script.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         DEAR Logout Script
 // @namespace    http://dearsystems.com/
-// @version      0.1.1
-// @description  This script will wait for a period of inactivity in DEAR and show a countdown. Then if there is an active sale, park the sale and logout, if there is no sale data, it will simply logout. This timeout will not occur on the payment screen. 
+// @version      0.2
+// @description  This script will wait for a period of inactivity in DEAR and show a countdown. Then if there is an active sale, park the sale and logout, if there is no sale data, it will simply logout. This timeout will not occur on the payment screen.
 // @author       Braydan Willrath & Andrey Tanner - andrew.wonderful@gmail.com - https://www.freelancer.com/u/awonderful
 // @match        https://pos.dearsystems.com/*
 // @grant        unsafeWindow
@@ -10,56 +10,37 @@
 
 (function() {
     'use strict';
-
     var InactivitySeconds=15; // Seconds to check inactivity
     var TimeoutSeconds=45; // Countdown seconds
-    var time,timeleft,dialog,countdownTimer,logouttimer,processing=0,_=function(id){return document.querySelector(id)},_q=function(id){return document.querySelectorAll(id)};
-
+    var time,timeleft,dialog,checkpayscreen,countdownTimer,logouttimer,processing=0,_=function(id){return document.querySelector(id)},_q=function(id){return document.querySelectorAll(id)};
 function onVisibilityChange(callback) {
     var visible = true;
     if (!callback){throw new Error('no callback given');}
     function focused(){if(!visible){callback(visible = true)}}
     function unfocused(){if(visible){callback(visible = false)}}
-
-    if ('hidden' in document) {
-        document.addEventListener('visibilitychange',
-            function() {(document.hidden ? unfocused : focused)()});
-    }
-    if ('mozHidden' in document) {
-        document.addEventListener('mozvisibilitychange',
-            function() {(document.mozHidden ? unfocused : focused)()});
-    }
-    if ('webkitHidden' in document) {
-        document.addEventListener('webkitvisibilitychange',
-            function() {(document.webkitHidden ? unfocused : focused)()});
-    }
-    if ('msHidden' in document) {
-        document.addEventListener('msvisibilitychange',
-            function() {(document.msHidden ? unfocused : focused)()});
-    }
-    if ('onfocusin' in document) {
-        document.onfocusin = focused;
-        document.onfocusout = unfocused;
-    }
-    window.onpageshow = window.onfocus = focused;
-    window.onpagehide = window.onblur = unfocused;
+    if ('hidden' in document){document.addEventListener('visibilitychange', function() {(document.hidden ? unfocused : focused)()});}
+    if ('mozHidden' in document){document.addEventListener('mozvisibilitychange', function() {(document.mozHidden ? unfocused : focused)()});}
+    if ('webkitHidden' in document){document.addEventListener('webkitvisibilitychange', function() {(document.webkitHidden ? unfocused : focused)()});}
+    if ('msHidden' in document) {document.addEventListener('msvisibilitychange', function() {(document.msHidden ? unfocused : focused)()});}
+    if ('onfocusin' in document) {document.onfocusin = focused;document.onfocusout = unfocused;}
+    window.onpageshow = window.onfocus = focused;window.onpagehide = window.onblur = unfocused;
 };
 function clickLogout(){
- var els=_q(".bs-menu li a span");for(var i=0;i<els.length;i++){if(els[i].textContent=='Logout'){els[i].click();}}
+ var els=_q(".bs-menu li a span");for(var i=0;i<els.length;i++){if(els[i].textContent=='Logout'){els[i].parentNode.click();_("#mainPart").click();window.setTimeout(function(){_(".dropdown").classList.remove('open')},2000);}} processing=0;
 }
-function triggerMouseEvent (node, eventType) {
+    function triggerMouseEvent (node, eventType) {
     var clickEvent = document.createEvent ('MouseEvents');
     clickEvent.initEvent (eventType, true, false);
     node.dispatchEvent (clickEvent);
-}
+    }
     function typeAndSend(){
         if(_('#saleNoteArea')){
            clearTimeout(dialog);
-           _('#saleNoteArea').value='';
-           window.setTimeout(function(){_('#saleNoteArea').dispatchEvent(new Event('change'));},300);
+           _('#saleNoteArea').value='Parked by DEAR Logout Script';
+           window.setTimeout(function(){_('#saleNoteArea').dispatchEvent(new Event('change'));},200);
            if(_("button[ng-mobile-click^='closeAddSaleNoteDialog(false)']")){
-              window.setTimeout(function(){var targetNode=_q("button[ng-mobile-click^='closeAddSaleNoteDialog(false)'] span")[2].querySelector("span");triggerMouseEvent(targetNode, "click");triggerMouseEvent(targetNode, "touchend");},500);
-              window.setTimeout(clickLogout,1000);
+              window.setTimeout(function(){var targetNode=_q("button[ng-mobile-click^='closeAddSaleNoteDialog(false)'] span")[2].querySelector("span");triggerMouseEvent(targetNode, "click");triggerMouseEvent(targetNode, "touchend");},400);
+              window.setTimeout(clickLogout,3500);
            }else{
              dialog=window.setTimeout(typeAndSend,500);
            }
@@ -67,7 +48,6 @@ function triggerMouseEvent (node, eventType) {
            dialog=window.setTimeout(typeAndSend,500);
         }
     }
-
     function setLogoutTimer(){
         if(_('#sellSummaryPartialView')){
           var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
@@ -81,9 +61,15 @@ function triggerMouseEvent (node, eventType) {
                     }
                   },(TimeoutSeconds*1000));
           }else{
-              console.log("We have payscreen true, clearing all");
-              window.clearTimeout(time);
-              window.clearTimeout(logouttimer);
+              if(!_('#saleSummaryCompleteID').classList.contains('ng-hide')){
+                  logouttimer=window.setTimeout(function(){
+                  _('button[ng-mobile-click^="endPayment()"] span').click();
+                  window.setTimeout(clickLogout,1000);
+                  },(TimeoutSeconds*1000));
+              }else{
+                  window.clearTimeout(time);
+                  window.clearTimeout(logouttimer);
+              }
           }
         }
     }
@@ -96,7 +82,7 @@ function triggerMouseEvent (node, eventType) {
     function xlogout(){
     if(_('#sellSummaryPartialView')&&_('#payLines')){
     var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
-    if(payscreen!='true'){
+    if(payscreen!='true'||!_('#saleSummaryCompleteID').classList.contains('ng-hide')){
         processing=1;
         clearTimeout(time);
         if(!_('#cntdwn')){
@@ -115,49 +101,62 @@ function triggerMouseEvent (node, eventType) {
      }
     }
     }
-function startTimer(){
-  if(_('#sellSummaryPartialView')){
-    var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
-    if(payscreen!='true'){
-      time=window.setTimeout(xlogout,(InactivitySeconds*1000));
-    }else{
-      window.clearTimeout(time);
-      window.clearTimeout(logouttimer);
+    function startTimer(){
+      if(_('#sellSummaryPartialView')){
+        var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
+        if(payscreen!='true'){
+          time=window.setTimeout(xlogout,(InactivitySeconds*1000));
+        }else{
+          if(!_('#saleSummaryCompleteID').classList.contains('ng-hide')){
+            time=window.setTimeout(xlogout,(InactivitySeconds*1000));
+          }else{
+            window.clearTimeout(time);
+            window.clearTimeout(logouttimer);
+          }
+        }
+      }
     }
-  }
-}
-function resetTimer(){
-    if(_('#cntdwn')){_('#cntdwn').remove();processing=0;}
-    window.clearTimeout(time);
-    window.clearTimeout(logouttimer);
-  if(_('#sellSummaryPartialView')){
-    var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
-    if(payscreen!='true'){
-    startTimer();
+    function resetTimer(){
+        if(_('#cntdwn')){_('#cntdwn').remove();processing=0;}
+        window.clearTimeout(time);
+        window.clearTimeout(logouttimer);
+      if(_('#sellSummaryPartialView')){
+        var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
+        if(payscreen!='true'){
+        startTimer();
+        }else{
+         if(!_('#saleSummaryCompleteID').classList.contains('ng-hide')){
+             startTimer();
+         }
+        }
+      }
     }
-  }
-}
-function setupTimers(){
-    document.addEventListener("mousemove", resetTimer, false);
-    document.addEventListener("mousedown", resetTimer, false);
-    document.addEventListener("keypress", resetTimer, false);
-    document.addEventListener("touchmove", resetTimer, false);
-    startTimer();
+function setupTimers(){document.addEventListener("mousemove",resetTimer,false);document.addEventListener("mousedown",resetTimer,false);document.addEventListener("keypress",resetTimer,false);document.addEventListener("touchmove",resetTimer,false);startTimer();}
+function checkpayfinished(){
+  checkpayscreen=setInterval(function(){if(!_('#saleSummaryCompleteID').classList.contains('ng-hide')){startTimer();clearInterval(checkpayscreen);}},1000);
 }
 onVisibilityChange(function(visible) {
    if(_('#sellSummaryPartialView')){
     if(!visible){
     var payscreen=_('#sellSummaryPartialView').getAttribute('aria-expanded');
        if(payscreen=='true'){
-           console.log("We have pay screen");
+           if(!_('#saleSummaryCompleteID').classList.contains('ng-hide')){
+               window.clearTimeout(time);
+               window.clearTimeout(logouttimer);
+               if(processing==0){
+               startTimer();
+               }
+           }
        }else{
-          window.clearTimeout(time);
+           window.clearTimeout(time);
            window.clearTimeout(logouttimer);
            if(processing==0){
                startTimer();
            }
        }
     }else{
+         clearInterval(checkpayscreen);
+         checkpayfinished();
          window.clearTimeout(time);
          window.clearTimeout(logouttimer);
          if(_('#cntdwn')){_('#cntdwn').remove();processing=0;}
@@ -165,4 +164,12 @@ onVisibilityChange(function(visible) {
    }
 });
     setupTimers();
+    window.setTimeout(function(){_(".user-name").style='display:inline-block';
+                                 _('.user-name-wrapper').style='padding-right:20px';
+                                 _('.user-name-wrapper img').style='width:15px;right:0;top:-25px;left:5px;position:relative;';
+                                 var lgbtn=document.createElement("div");
+                                 lgbtn.id='lgbtn';
+                                 lgbtn.style='position:absolute;opacity:0.9;display:block;width:24px;height:24px;background-image:url(https://cdn2.iconfinder.com/data/icons/metro-uinvert-dock/256/Power_-_Switch_User.png);filter:invert(100%);background-size:contain;top:17px;right:10px;cursor:pointer;';
+                                 lgbtn.onclick=function(){clickLogout()};
+                                 _('.btn-group').appendChild(lgbtn)},2000);
 })();
